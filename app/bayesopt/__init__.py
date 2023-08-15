c0ee662e43dfc69698774e4182f672002e7a248d
wbjin <wbjin@umich.edu>
2023-08-16 00:15:06 +0900

    Two param optimization page

--- app/bayesopt/__init__.py
+++ app/bayesopt/__init__.py
@@ -1,13 +1,9 @@
-import os
 from flask import Flask, render_template, request
 from . import config
 from . import demoOp
 from . import oneOpt
 from . import twoOpt
-from . import threeOpt
-from . import fourOpt
 from .dashapp import upload
-from dash import Dash
 
 def createApp():
     app = config.app
@@ -16,8 +12,6 @@ def createApp():
     app.register_blueprint(demoOp.blueprint)
     app.register_blueprint(oneOpt.blueprint)
     app.register_blueprint(twoOpt.blueprint)
-    app.register_blueprint(threeOpt.blueprint)
-    app.register_blueprint(fourOpt.blueprint) 
 
     return app
 
